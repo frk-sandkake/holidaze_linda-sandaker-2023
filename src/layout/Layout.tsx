542a99aa40b38589efbca5d61535798b565ef317
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2023-05-22 19:11:40 +0200

    Layout, w Header&Footer added and working

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import { Footer } from "./Footer";
+import { Outlet as OutletRrd } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+
+function Layout() {
+    return (
+        <>
+            <Header/>
+            <Container className="py-4 vh-100" h-100>
+                <OutletRrd/>
+            </Container>
+            <Footer/>
+        </>
+    )
+}
+
+export default Layout
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react"
-import { Button, Container } from "react-bootstrap"
+import { Button } from "react-bootstrap"
 
 export default function Home() {
     const [count, setCount] = useState(0)
 
     return (
         <div>
-            <Container>
             <h1>Hello Home page!</h1>
             <Button type='button' className="fw-bold" variant="primary" onClick={() => setCount((count) => count + 1)}>
                 count is {count}
             </Button>
             <p>I'm a paragraph</p>
-            </Container>
         </div>
     )
 }
